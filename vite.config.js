44ae062f664d6f41710e08535413775dd0494615
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2023-07-16 11:04:36 -0300

    🔧 Config server and preview port

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 export default defineConfig({
+  server: {
+    port: 4000,
+  },
+  preview: {
+    port: 4001,
+  },
   plugins: [react()],
   resolve: {
     alias: [
